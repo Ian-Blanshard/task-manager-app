ee6137679b994e319a28ce04411d847b796e2515
ianb <ian246@hotmail.co.uk>
2024-08-04 11:50:20 +0100

    added functionality for adding tasks

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -3,3 +3,14 @@ document.addEventListener('DOMContentLoaded', function() {
     let sidenav = document.querySelectorAll('.sidenav');
     M.Sidenav.init(sidenav);
   });
+
+  document.addEventListener('DOMContentLoaded', function() {
+    let datepicker = document.querySelectorAll('.datepicker');
+    M.Datepicker.init(datepicker, {
+        format: "dd mmmm, yyyy",
+        i18n: {done: "Select"}
+    });
+
+    let selects = document.querySelectorAll('select');
+    M.FormSelect.init(selects);
+  });
